aab7fb50c5ccd2fa600c9b1fbce4e040f2ed7c75
chukwuebuka <okolisamuel21@gmail.com>
2025-03-18 10:26:50 +0100

    create the compute module
    create a compute module.., accept tags and instance count.., add Server:
    Jekinns tags to one instance when instance count in more than one

---
+++ terraform/modules/compute/ec2/main.tf
@@ -0,0 +1,25 @@
+# modules/compute/main.tf
+
+resource "aws_instance" "main_ec2" {
+  count                       = var.instance_count
+  ami                         = "ami-04b4f1a9cf54c11d0"
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = var.security_groups
+  associate_public_ip_address = var.assign_ip
+  key_name                    = var.key_name
+  tags = merge(
+    var.tags,
+    {
+      "Server" = var.instance_count > 1 && count.index == 0 ? var.apps[0] : var.apps[1]
+    }
+  )
+
+  root_block_device {
+    volume_size = var.volume_size # Increase to 30GB (or adjust as needed)
+    volume_type = var.volume_type # Use gp3 for better performance
+  }
+}
+
+
+# Load Balancers 
